84510627dbaf62707ea0bf7dfc8c02c5281c013d
ScarapNv <nr.nathanrafael@gmail.com>
2024-10-08 20:58:29 +0700

    ScriptsLuaUpdate

--- Scripts.lua
+++ Scripts.lua
@@ -1,14 +1,4 @@
-local function SetLas(Test, Das)
-    print(Test)  -- Menampilkan nilai Test
-    print(Das)   -- Menampilkan nilai Das
-end
-
-SetLas(12, 10)   -- Memanggil fungsi dengan argumen 12 dan 10
-
-local TestTabel = {
-    "Test1"   -- Elemen tabel pertama
+local Testtabel = {
+    "test1",
+    "Test2"
 }
-
-for _, Text in pairs(TestTabel) do
-    print(Text)   -- Menampilkan setiap elemen dari tabel
-end
