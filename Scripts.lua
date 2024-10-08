7633e90bab77581ae9738cf4d397652980688f27
ScarapNv <nr.nathanrafael@gmail.com>
2024-10-08 20:41:05 +0700

    Idk
    las

---
+++ Scripts.lua
@@ -0,0 +1,14 @@
+local function SetLas(Test, Das)
+    print(Test)  -- Menampilkan nilai Test
+    print(Das)   -- Menampilkan nilai Das
+end
+
+SetLas(12, 10)   -- Memanggil fungsi dengan argumen 12 dan 10
+
+local TestTabel = {
+    "Test1"   -- Elemen tabel pertama
+}
+
+for _, Text in pairs(TestTabel) do
+    print(Text)   -- Menampilkan setiap elemen dari tabel
+end
