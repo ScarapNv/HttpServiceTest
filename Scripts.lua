60ff7827fd6b8bd6fc239a4f74b2057ab7bdabf5
ScarapNv <nr.nathanrafael@gmail.com>
2024-10-08 21:06:59 +0700

    LAAAAAAAAAAAAAAA

--- Scripts.lua
+++ Scripts.lua
@@ -1,14 +1,19 @@
+-- Scripts.lua
 local function SetLas(Test, Das)
     print(Test)  -- Menampilkan nilai Test
     print(Das)   -- Menampilkan nilai Das
 end
 
-SetLas(12, 10)   -- Memanggil fungsi dengan argumen 12 dan 10
+local function Run()
+    SetLas(12, 10)  -- Memanggil fungsi dengan argumen 12 dan 10
 
-local TestTabel = {
-    "Test1"   -- Elemen tabel pertama
-}
+    local TestTabel = {
+        "Test1"  -- Elemen tabel pertama
+    }
 
-for _, Text in pairs(TestTabel) do
-    print(Text)   -- Menampilkan setiap elemen dari tabel
+    for _, Text in pairs(TestTabel) do
+        print(Text)  -- Menampilkan setiap elemen dari tabel
+    end
 end
+
+return Run -- Mengembalikan fungsi Run
